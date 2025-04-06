310a7b8eedb26ac646e0c2654a1b759302826dea
Endrit <endritkastrati8@gmail.com>
2025-04-07 00:32:24 +0200

    Final touches in login - Language Switch

--- src/custom-modules/Login.js
+++ src/custom-modules/Login.js
@@ -1,34 +1,56 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
-import Emblem from './Emblem'
-import TranslateMenu from './TranslateMenu.js'
+import Emblem from './Emblem';
+import LanguageSwitcher from '../components/LanguageSwitcher';
 
-import '../css/login.css'
-import '../css/index.css'
+import '../css/login.css';
+import '../css/index.css';
 
 function Login() {
-  const [email, setEmail] = useState('')
-  let [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { t } = useTranslation();
   
-  const handleLogin = (e) =>{
-    e.preventDefault()
-    if (email == '' || password == ''){
-      alert("Plotsoj krejt fushat.")
-      return
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (email === '' || password === '') {
+      alert(t('fill_all_fields'));
+      return;
     }
-      alert("E bone login me te dhenat:" + email + " dhe " + password)
+    alert(t('login_attempt', { email, password }));
   }
   
   return (
     <div className='login-page'>
-      <TranslateMenu />
+      <LanguageSwitcher />
       <form onSubmit={handleLogin} id='login-form'>
         <Emblem />
-        <label>Perdoruesi: <input type='number' pattern='[0-9]*' value={email} onChange={(e) => {setEmail(e.target.value)}}/></label><br/>
-        <label>Fjalekalimi: <input type='password' value={password} onChange={(e) => {setPassword(e.target.value)}}/></label><br/>
+        <label>
+          {t('username')}: 
+          <input 
+            type='number' 
+            pattern='[0-9]*' 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br/>
+        <label>
+          {t('password')}: 
+          <input 
+            type='password' 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br/>
         
-        <a href='/'>Keni harruar fjalekalimin?</a>
-        <button className='accent-button' type='submit'>Kycu</button><br/>
+        <a href='/'>{t('forgot_password')}</a>
+        <button className='accent-button' type='submit'>
+          {t('login_button')}
+        </button>
+        <br/>
       </form>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
 
 import Login from './custom-modules/Login';
-
 import './css/index.css';
-
 import reportWebVitals from './reportWebVitals';
+import i18n from './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-/*
-
-TODO:
-1. me bo me router (mos mu refresh nmes t hrefave)
-// gjithqysh kqyre Link per router se sbon me href 
-  
-*/
-
-
-// kqyre session, nese null, bire qitu
 root.render(
   <React.StrictMode>
-    <Login />
+    <I18nextProvider i18n={i18n}>
+      <Router>
+        <Login />
+      </Router>
+    </I18nextProvider>
   </React.StrictMode>
 );
-// nese jo, qoje te home
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
