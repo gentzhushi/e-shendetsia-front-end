d72b2913c12d98404e6a74542a9aece113078fe2
endrit <endritkastrati8@gmail.com>
2025-05-08 01:55:25 +0200

    Path adding to main file .js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,15 @@ import Login from './custom-modules/Login';
 import './css/index.css';
 import reportWebVitals from './reportWebVitals';
 import i18n from './i18n';
+
 import HelloFromBackend from './custom-modules/HelloFromBackend';
-import Dashboard from './custom-modules/clinics/clinics_dashboard';
-import Settings from './custom-modules/clinics/clinics_settings';
+
+import ClinicsDashboard from './custom-modules/clinics/clinics_dashboard';
+import ClinicsSettings from './custom-modules/clinics/clinics_settings';
+
+import PatientsDashboard from './custom-modules/patients/patients_dashboard';
+import PatientsAppointments from './custom-modules/patients/patients_appointments';
+import PatientsProfile from './custom-modules/patients/patients_profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -21,8 +27,12 @@ root.render(
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/api/hello' element={<HelloFromBackend />} />
-          <Route path='/clinics_dashboard' element={<Dashboard />} />
-          <Route path='/clinics_settings' element={<Settings />} />
+          <Route path='/clinics_dashboard' element={<ClinicsDashboard />} />
+          <Route path='/clinics_settings' element={<ClinicsSettings />} />
+
+          <Route path='/patients_dashboard' element={<PatientsDashboard />} />
+          <Route path='/patients_appointments' element={<PatientsAppointments />} />
+          <Route path='/patients_profile' element={<PatientsProfile />} />
         </Routes>
       </Router>
     </I18nextProvider>
